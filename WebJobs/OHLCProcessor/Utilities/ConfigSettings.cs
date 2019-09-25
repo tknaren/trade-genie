914ea98e342cddef16c01a6d34a9b8e2f67c18cb
Naren Krishnamurthy <naren.x.krishnamurthy@gsk.com>
2019-09-25 21:04:04 +0530

    Moved all appsettings to Cloud configuration manager

--- WebJobs/OHLCProcessor/Utilities/ConfigSettings.cs
+++ WebJobs/OHLCProcessor/Utilities/ConfigSettings.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
 using Serilog;
+using Microsoft.Azure;
 
 namespace Utilities
 {
@@ -14,8 +15,11 @@ namespace Utilities
         {
             get
             {
-                return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["StartingTimeHour"]),
-                                             Int32.Parse(ConfigurationManager.AppSettings["StartingTimeMinute"]), 0);
+                return new TimeSpan(Int32.Parse(CloudConfigurationManager.GetSetting("StartingTimeHour")),
+                                             Int32.Parse(CloudConfigurationManager.GetSetting("StartingTimeMinute")), 0);
+
+                //return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["StartingTimeHour"]),
+                //                             Int32.Parse(ConfigurationManager.AppSettings["StartingTimeMinute"]), 0);
             }
         }
 
@@ -23,8 +27,11 @@ namespace Utilities
         {
             get
             {
-                return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["EndingTimeHour"]),
-                                             Int32.Parse(ConfigurationManager.AppSettings["EndingTimeMinute"]), 0);
+                return new TimeSpan(Int32.Parse(CloudConfigurationManager.GetSetting("EndingTimeHour")),
+                                             Int32.Parse(CloudConfigurationManager.GetSetting("EndingTimeMinute")), 0);
+
+                //return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["EndingTimeHour"]),
+                //                             Int32.Parse(ConfigurationManager.AppSettings["EndingTimeMinute"]), 0);
             }
         }
 
@@ -32,69 +39,84 @@ namespace Utilities
         {
             get
             {
-                return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["HistoryEndTimeHour"]),
-                                             Int32.Parse(ConfigurationManager.AppSettings["HistoryEndTimeMinute"]), 0);
+                return new TimeSpan(Int32.Parse(CloudConfigurationManager.GetSetting("HistoryEndTimeHour")),
+                                             Int32.Parse(CloudConfigurationManager.GetSetting("HistoryEndTimeMinute")), 0);
+
+                //return new TimeSpan(Int32.Parse(ConfigurationManager.AppSettings["HistoryEndTimeHour"]),
+                //                             Int32.Parse(ConfigurationManager.AppSettings["HistoryEndTimeMinute"]), 0);
             }
         }
 
         public string Exchange
         {
-            get { return ConfigurationManager.AppSettings["Exchange"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Exchange").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Exchange"].ToString(); }
         }
 
         public string PeriodInDays
         { 
-            get { return ConfigurationManager.AppSettings["PeriodInDays"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("PeriodInDays").ToString(); }
+            //get { return ConfigurationManager.AppSettings["PeriodInDays"].ToString(); }
         }
 
         public string DelayInMin
         {
-            get { return ConfigurationManager.AppSettings["DelayInMin"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("DelayInMin").ToString(); }
+            //get { return ConfigurationManager.AppSettings["DelayInMin"].ToString(); }
         }
 
         public string IntervalInMin
         {
-            get { return ConfigurationManager.AppSettings["IntervalInMin"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("IntervalInMin").ToString(); }
+            //get { return ConfigurationManager.AppSettings["IntervalInMin"].ToString(); }
         }
 
         public string APIKey
         {
-            get { return ConfigurationManager.AppSettings["Api_Key"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Api_Key").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Api_Key"].ToString(); }
         }
 
         public string APISecret
         {
-            get { return ConfigurationManager.AppSettings["API_Secret"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("API_Secret").ToString(); }
+            //get { return ConfigurationManager.AppSettings["API_Secret"].ToString(); }
         }
 
         public string RedirectUrl
         {
-            get { return ConfigurationManager.AppSettings["Redirect_Url"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Redirect_Url").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Redirect_Url"].ToString(); }
         }
 
         public string UpstoxBaseUri
         {
-            get { return ConfigurationManager.AppSettings["UpstoxBaseUri"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("UpstoxBaseUri").ToString(); }
+            //get { return ConfigurationManager.AppSettings["UpstoxBaseUri"].ToString(); }
         }
 
         public string HistoricalAPI
         {
-            get { return ConfigurationManager.AppSettings["HistoricalAPI"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("HistoricalAPI").ToString(); }
+            //get { return ConfigurationManager.AppSettings["HistoricalAPI"].ToString(); }
         }
 
         public string UserAgent
         {
-            get { return ConfigurationManager.AppSettings["UserAgent"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("UserAgent").ToString(); }
+            //get { return ConfigurationManager.AppSettings["UserAgent"].ToString(); }
         }
 
         public string StartDate
         {
-            get { return ConfigurationManager.AppSettings["StartDate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("StartDate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["StartDate"].ToString(); }
         }
 
         public string EndDate
         {
-            get { return ConfigurationManager.AppSettings["EndDate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("EndDate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["EndDate"].ToString(); }
         }
 
         public string AzSQLConString
@@ -104,82 +126,98 @@ namespace Utilities
 
         public string Min3Timer
         {
-            get { return ConfigurationManager.AppSettings["Min3Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min3Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min3Timer"].ToString(); }
         }
 
         public string Min5Timer
         {
-            get { return ConfigurationManager.AppSettings["Min5Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min5Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min5Timer"].ToString(); }
         }
 
         public string Min10Timer
         {
-            get { return ConfigurationManager.AppSettings["Min10Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min10Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min10Timer"].ToString(); }
         }
 
         public string Min15Timer
         {
-            get { return ConfigurationManager.AppSettings["Min15Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min15Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min15Timer"].ToString(); }
         }
 
         public string Min30Timer
         {
-            get { return ConfigurationManager.AppSettings["Min30Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min30Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min30Timer"].ToString(); }
         }
 
         public string Min60Timer
         {
-            get { return ConfigurationManager.AppSettings["Min60Timer"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("Min60Timer").ToString(); }
+            //get { return ConfigurationManager.AppSettings["Min60Timer"].ToString(); }
         }
 
         public bool IsPositional
         {
-            get { return Convert.ToBoolean(ConfigurationManager.AppSettings["IsPositional"].ToString()); }
+            get { return Convert.ToBoolean(CloudConfigurationManager.GetSetting("IsPositional").ToString()); }
+            //get { return Convert.ToBoolean(ConfigurationManager.AppSettings["IsPositional"].ToString()); }
         }
 
         public string TimePeriodsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["TimePeriodsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("TimePeriodsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["TimePeriodsToCalculate"].ToString(); }
         }
 
         public string EMAsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["EMAsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("EMAsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["EMAsToCalculate"].ToString(); }
         }
 
         public string EHEMAsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["EHEMAsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("EHEMAsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["EHEMAsToCalculate"].ToString(); }
         }
 
         public string VWMAsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["VWMAsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("VWMAsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["VWMAsToCalculate"].ToString(); }
         }
 
         public string RSIsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["RSIsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("RSIsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["RSIsToCalculate"].ToString(); }
         }
 
         public string ForceIndexesToCalculate
         {
-            get { return ConfigurationManager.AppSettings["ForceIndexesToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("ForceIndexesToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["ForceIndexesToCalculate"].ToString(); }
         }
 
         public string ATRsToCalculate
         {
-            get { return ConfigurationManager.AppSettings["ATRsToCalculate"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("ATRsToCalculate").ToString(); }
+            //get { return ConfigurationManager.AppSettings["ATRsToCalculate"].ToString(); }
         }
 
         public string SuperTrendMultipliers
         {
-            get { return ConfigurationManager.AppSettings["SuperTrendMultipliers"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("SuperTrendMultipliers").ToString(); }
+            //get { return ConfigurationManager.AppSettings["SuperTrendMultipliers"].ToString(); }
         }
 
         public string EMADeviationPeriods
         {
-            get { return ConfigurationManager.AppSettings["EMADeviationPeriods"].ToString(); }
+            get { return CloudConfigurationManager.GetSetting("EMADeviationPeriods").ToString(); }
+            //get { return ConfigurationManager.AppSettings["EMADeviationPeriods"].ToString(); }
         }
     }
 }
