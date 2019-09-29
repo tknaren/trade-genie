9d86e52f655beb85b111da9d959986e82304a99c
Naren <tknaren@gmail.com>
2019-09-29 22:25:44 +0530

    Added Back Testing Project

---
+++ WindowsApp/BackTestingApp/DataAccessLayer/DBMethods.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class DBMethods
+    {
+        public List<MasterStockList> GetMasterStockList()
+        {
+            List<MasterStockList> masterStocks = new List<MasterStockList>();
+
+            using (TGEntities tgEntities = new TGEntities())
+            {
+
+                var masterStockList = from msl in tgEntities.MasterStockLists
+                                      where msl.IsIncluded == true
+                                      select msl;
+
+                masterStocks = masterStockList.ToList();
+            }
+
+            return masterStocks;
+        }
+
+        public List<TickerMin> GetMinuteData(DateTime startDate, DateTime endDate, string stockCode)
+        {
+            List<TickerMin> minDatalist = new List<TickerMin>();
+
+            using (TGEntities tgEntities = new TGEntities())
+            {
+                var tickerMinList = from tm in tgEntities.TickerMins
+                                    where tm.TradingSymbol == stockCode
+                                          && tm.DateTime >= startDate
+                                          && tm.DateTime <= endDate
+                                    orderby tm.DateTime ascending
+                                    select tm;
+
+                minDatalist = tickerMinList.ToList();
+            }
+
+            return minDatalist;
+        }
+
+        public List<spGetGapOpenedScripts_Result> GetStocksWithGapOpening(DateTime yesterday, DateTime today, 
+                                        int targetPercentage, int gapPercentage, int priceRangeHigh, int priceRangeLow)
+        {
+            List<spGetGapOpenedScripts_Result> listOfStocks = new List<spGetGapOpenedScripts_Result>();
+
+            using (TGEntities tgEntities = new TGEntities())
+            {
+                listOfStocks = tgEntities.spGetGapOpenedScripts(yesterday, today, targetPercentage, gapPercentage, priceRangeHigh, priceRangeLow).ToList<spGetGapOpenedScripts_Result>();
+            }
+
+            return listOfStocks;
+        }
+    }
+}
